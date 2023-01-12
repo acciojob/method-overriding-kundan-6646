2150536b4289d160b8efd6b990583e1c52f9f148
unknown <kundanjha826@gmail.com>
2023-01-12 23:31:01 +0530

    third-commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,15 +6,3 @@ public class Main {
   }
 }
 
-class A {
-  public String meth() {
-    return "Invoking method from class A";
-  }
-}
-
-class B extends A {
-  @Override
-  public String meth() {
-    return "Method is overridden in Extendend class B";
-  }
-}
---
+++ src/test/java/com/driver/test/A.java
@@ -0,0 +1,7 @@
+package com.driver.test;
+
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
